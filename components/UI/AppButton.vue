9ec357b10a590380c1e0c63b56b0382cf80f052e
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-04-25 15:24:04 +0300

    Admin section added & the form for new-post with simple validation

---
+++ components/UI/AppButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-btn
+    v-bind="$attrs"
+    v-on="$listeners"
+    :color="color"
+    :to="to"
+    nuxt
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+  export default {
+    name: 'AppButton',
+    props: {
+      color: {
+        type: String,
+        required: false,
+        default: 'info'
+      },
+      to: {
+        type: String,
+        required: false
+      }
+    }
+  }
+</script>
---
+++ components/UI/AppInput.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div>
+      <v-text-field
+        :label="label"
+        :multi-line="controlType === 'textarea'"
+        :name="name"
+        :rules="rules"
+        :value="value"
+        @input="$emit('input', $event)"
+      >
+      </v-text-field>
+    </div>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'AppInput',
+    props: {
+      controlType: {
+        type: String,
+        required: false,
+      },
+      label: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      value: {
+        type: String
+      },
+      rules: {
+        type: Array,
+        required: false
+      }
+    }
+  }
+</script>
