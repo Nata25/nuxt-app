c1937d0a5d5404e0bddc3e3382ea2805b87f1a48
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-05-02 15:22:56 +0300

    Error component

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-layout class="wrapper" row wrap justify-center error--text>
+      Sorry, something went wrong! 😱
+    </v-layout>
+
+    <v-layout justify-center>
+      <v-btn href="/">
+        Go to home page &rarr;
+      </v-btn>  
+    </v-layout>
+
+  </v-container>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    padding-top: 100px;
+    height: 30vh;
+    font-size: 3em;
+  }
+
+</style>
